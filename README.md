c7479ebe770c94feabf50d5766c718970b6961d8
andypixel <andrewpixel@gmail.com>
2014-10-05 13:59:20 -0700

    Added local_settings.js

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ geotracks
 =========
 
 Node console app to pull data from Geoloqi and save to text files
+
+Setting up
+----------
+Make a copy of `local_settings.js.template` and rename to `local_settings.js`. Edit to include your application's settings.
--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var fs = require('fs');
+var geoloqi = require('geoloqi');
+var settings = require('./local_settings')
+
+//##TODO pass this in command line, or use private config file
+var session = new geoloqi.Session({'access_token': settings.oauth});
+
 
 var app = {
 	init: function() {
-		fs.appendFile('output/test.txt', (new Date()).getSeconds() + '\r\n', function (err) {
-			if (err) {
-				console.log(err)
+		// fs.appendFile('output/test.txt', (new Date()).getSeconds() + '\r\n', function (err) {
+		// 	if (err) {
+		// 		console.log(err)
+		// 	} else {
+		// 		console.log('success')
+		// 	}
+		// });
+
+		session.get('/location/history?count=10', function(result, err) {
+			if(err) {
+				throw new Error('There has been an error! '+err);
 			} else {
-				console.log('success')
+				console.log(result);
 			}
 		});
 	}
